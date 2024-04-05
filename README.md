7b59f008b7fb03a56e980cf55517ea7a2155e187
kovascmate <kmate0630@gmail.com>
2024-04-06 00:52:03 +0200

    To do task: If one gmail window is already open, just switch that tab.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-To do: - If one gmail window is already open, just switch that tab.
-       - Show on the icon how many unread e-mails there are in the inbox.
+To do: - Show on the icon how many unread e-mails there are in the inbox.
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({ url: 'https://mail.google.com/' });
+chrome.browserAction.onClicked.addListener(function(tab) { 
+			// Check if there's an existing Gmail tab 
+			chrome.tabs.query({ url: "https://mail.google.com/*", currentWindow: true }, function(tabs) 
+			{ if (tabs.length > 0){
+				// If a Gmail tab is already open, switch to it 
+			chrome.tabs.update(tabs[0].id, { active: true }); 
+			} else { 
+				// If no Gmail tab is open, create a new one 
+			chrome.tabs.create({ url: 'https://mail.google.com/' 
+			});
+		} 
+	}); 
 });
